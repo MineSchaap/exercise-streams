1bdb3af521ccd0596be1c57152c4783f5534df9d
MineSchaap <52737887+MineSchaap@users.noreply.github.com>
2023-09-07 12:00:33 +0200

    fdfdf
    fdfd

--- src/main/java/nl/han/aim/oose/dea/StreamsCalculateTotalCost.java
+++ src/main/java/nl/han/aim/oose/dea/StreamsCalculateTotalCost.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class StreamsCalculateTotalCost {
     int calculateTotalCostOfAllProducts(List<Product> products) {
         // TODO: implement
+        System.out.println(products.get(0).getPrice());
+
+        products.stream().;
 
         return 0;
     }
